4bdf00a0698fca2288297198f6789c8d6f3f9175
JesusLinares01 <albertolinares001@gmail.com>
2019-07-11 12:37:44 -0600

    methods get and posts

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-
+import json
 # Init App
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ class Product(db.Model):
 	price = db.Column(db.Float)
 	qty = db.Column(db.Integer)
 
-	def __ini__(self, name, description, price, qty):
+	def __init__(self, name, description, price, qty):
 		self.name = name
 		self.description = description
 		self.price = price
@@ -44,17 +44,24 @@ products_schema = ProductSchema(many = True, strict = True)
 # Create a Product
 @app.route("/product", methods = ['POST'])
 def add_product():
+	
 	name = request.json['name']
 	description = request.json['description']
 	price = request.json['price']
 	qty = request.json['qty']
+	
+	new_product = Product(name, description, price, qty)
 
-new_product = Product(name, description, price, qty)
+	db.session.add(new_product)
+	db.session.commit()
 
-db.session.add(new_product)
-db.session.commit()
+	return product_schema.jsonify(new_product)
+@app.route("/product", methods = ["GET"])
+def get_product():
+	all_products = Product.query.all()
+	result = products_schema.dump(all_products)
 
-return product_schema.jsonify(new_product)
+	return jsonify(result.data)
 
 # Run Server
 if __name__ == "__main__":
